8dcde2ad053a096fca759639cf77e0ca9ac19226
sdlambert <schm01978@gmail.com>
2016-10-23 13:11:49 -0400

    Finished todo app tutorial

--- js/todo.js
+++ js/todo.js
@@ -13,9 +13,44 @@
 
 function start() {
 	new Vue({
-		el: "#todo",
+		el  : "#todo",
 		data: {
-			test: "Woohoo!"
+			newTask: "",
+			taskList: []
+		},
+		computed: {
+			allAreSelected: function() {
+				return this.taskList.every(function(task) {
+					return task.checked;
+				}) && this.taskList.length > 0;
+			}
+		},
+		methods: {
+			addTask: function() {
+				var task = this.newTask.trim();
+
+				if (task) {
+					this.taskList.push({
+						text: task,
+						checked: false
+					});
+
+					this.newTask = "";
+				}
+			},
+			removeTask: function(task) {
+				var index = this.taskList.indexOf(task);
+				this.taskList.splice(index, 1);
+			},
+			clearList: function() {
+				this.taskList = [];
+			},
+			selectAll: function() {
+				var targetValue = this.allAreSelected ? false : true;
+				for (var i = 0; i < this.taskList.length; i++) {
+					this.taskList[i].checked = targetValue;
+				}
+			}
 		}
 	});
 }
